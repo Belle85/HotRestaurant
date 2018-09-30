07c648be4893dac24efd8fea2347a92ef0aeb73b
Belle <bebold.bebelle@gmail.com>
2018-09-29 23:42:50 -0400

    Added POST routes and some footer css.

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -11,4 +11,22 @@ module.exports = function(app){
         res.json(waitingListData);
         console.log(waitingListData);
     });
+
+    app.post('/api/tables', function(req, res){
+        if(tableData.length < 5){
+            tableData.push(req.body);
+            res.json(true);
+        } else{
+            waitingListData.push(req.body);
+            res.json(false);
+        };
+    });
+
+    app.post('/api/clear', function(){
+        tableData = [];
+        waitingListData = [];
+
+        console.log(tableData);
+        console.log(waitingListData);
+    });
 };
