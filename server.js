3cbe6e46cb23b1c4b5d6e94957b229208bc6cad5
Belle <bebold.bebelle@gmail.com>
2018-09-29 21:36:55 -0400

    Debugging the routes.

--- server.js
+++ server.js
@@ -15,18 +15,14 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
  
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted:\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-// });
+
 
 //ROUTES
 //====================================================
 
 
 // app.get('/', (req, res) => res.send('Hello World!'))
-
+require('./app/routing/html-routes.js')(app);
 
 //APP LISTENING
 //====================================================
